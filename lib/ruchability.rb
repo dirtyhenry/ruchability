cfcfaea751dcf9e66fd2cdc0b2e0ae9bf9f96364
Mickaël Floc’hlay <mickael.flochlay@memo.bank>
2021-10-28 11:14:23 +0200

    2021 Maintenance before archiving

--- lib/ruchability.rb
+++ lib/ruchability.rb
@@ -1,35 +1,32 @@
-require "ruchability/version"
+# frozen_string_literal: true
+
+require 'ruchability/version'
 require 'net/http'
 require 'benchmark'
 require 'date'
 
 module Ruchability
-
   class Reacher
-
-    # 
-    def reach host = 'www.google.com', port = 80, open_timeout = 5
+    def reach(host = 'www.google.com', port = 80, open_timeout = 5)
       puts "Testing with #{host} on port #{port}"
       begin
         res = { version: Ruchability::VERSION, date: DateTime.now.to_s, host: host, port: port }
         response = nil
         bm = Benchmark.measure do
-          Net::HTTP.start(host, port, nil, nil, nil, nil, { open_timeout: open_timeout } ) do |http|
+          Net::HTTP.start(host, port, nil, nil, nil, nil, { open_timeout: open_timeout }) do |http|
             response = http.head('/')
           end
         end
         res[:test] = { status: 0, response_code: response.code, total_time: bm.total, real_time: bm.real }
-      rescue => e
+      rescue StandardError => e
         res[:test] = { status: 1, error: e }
       end
-      puts "#{res}"
+      puts res.to_s
       res
     end
-  
   end
-  
+
   class AgentFactory
-    def xml
-    end
+    def xml; end
   end
 end
